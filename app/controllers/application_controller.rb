462acee9ab682f0cf9201d16f111374b8edea7be
Steffen Tröster <stetro.freaky@googlemail.com>
2012-03-27 22:19:09 +0200

    HTTP Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+before_filter :authenticate
+
   protect_from_forgery
   def index
     redirect_to(people_url)
+  end
+  
+protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "foo" && password == "bar"
+    end
   end
 end
