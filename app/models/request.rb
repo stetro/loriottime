e0c37cfe7e44c8ad2a5c8c14ca1d63772435181a
Steffen Tröster <stetro.freaky@googlemail.com>
2012-03-27 22:01:48 +0200

    Umlaute entfernt

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,5 +3,5 @@ class Request < ActiveRecord::Base
   belongs_to :event
   belongs_to :phase
   validates_presence_of :person_id, :message => "Bitte geben Sie eine Person an !"
-  validates_presence_of :seat_count , :message => "Bitte wählen Sie die Anzahl der Pl&auml;tze aus !"
+  validates_presence_of :seat_count , :message => "Bitte w&auml;hlen Sie die Anzahl der Pl&auml;tze aus !"
 end
